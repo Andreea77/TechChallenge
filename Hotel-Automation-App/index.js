7238a0fc420d69565c2b72c1c5faca6703c1833a
Amurov Constantin <amurov.constantin@gmail.com>
2021-03-29 17:57:14 +0300

    [add] Added plain home-page.ejs file

--- Hotel-Automation-App/index.js
+++ Hotel-Automation-App/index.js
@@ -1,35 +1,30 @@
-
-var express = require('express');
-var mongoose = require('mongoose');
+var express = require("express");
+var mongoose = require("mongoose");
 var app = express();
 const https = require("https");
 const bodyParser = require("body-parser");
 //Set view engine to EJS
-app.set('view engine','ejs');
-app.use(express.static(__dirname + '/public'))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 //Connect to MongoDB
-mongoose.connect('mongodb://localhost/testDB');
+mongoose.connect("mongodb://localhost/testDB");
 //Verify connection
-mongoose.connection.once('open',function() {
-      console.log("Connection  to DB has been made");
-}).on('error',function() {
-    console.log('Connection to DB error');
+mongoose.connection
+  .once("open", function () {
+    console.log("Connection  to DB has been made");
+  })
+  .on("error", function () {
+    console.log("Connection to DB error");
+  });
+
+app.get("/login", function (req, res) {
+  res.render("login");
+
+  // si la app.get('/:userQuery'');
+  //res.render('testEJS',{ data:  {userQuery: req.params.userQuery,  searchResults : ['Book1','Book2','Book3']) }} ;
 });
-
-
-app.get('/login', function (req, res) {
- res.render('login');
- 
-
-    // si la app.get('/:userQuery'');
-    //res.render('testEJS',{ data:  {userQuery: req.params.userQuery,  searchResults : ['Book1','Book2','Book3']) }} ;
- 
-
-});
-
-
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log("Example app listening on port 3000!");
 });
