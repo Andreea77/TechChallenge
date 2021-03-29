e31ed0931d0f7e6e2e854a3357f474372003517a
Amurov Constantin <amurov.constantin@gmail.com>
2021-03-29 22:04:35 +0300

    [ADD/CHANGE] Added security code page and name attributes.

--- Hotel-Automation-App/index.js
+++ Hotel-Automation-App/index.js
@@ -8,12 +8,14 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
 //Connect to MongoDB
-mongoose.connect("mongodb+srv://dbuser:qwe123@cluster0.ueb1t.mongodb.net/hotel?retryWrites=true&w=majority");
+mongoose.connect(
+  "mongodb+srv://dbuser:qwe123@cluster0.ueb1t.mongodb.net/hotel?retryWrites=true&w=majority"
+);
 //Verify connection
 mongoose.connection
   .once("open", function () {
@@ -31,31 +33,32 @@ var User = require("./models/user");
 
 //Showing login form
 app.get("/login", function (req, res) {
-	res.render("login");
+  res.render("login");
+});
+app.get("/security-code", function (req, res) {
+  res.render("security-code");
 });
 
 app.post("/login", function (req, res) {
-  var _username = req.body.username
-	var _password = req.body.password
-	User.find({}, function(err, profiles) {
-		console.log(profiles);
-	 });
+  var _username = req.body.username;
+  var _password = req.body.password;
+  User.find({}, function (err, profiles) {
+    console.log(profiles);
+  });
 
-	User.findOne({username: _username}, function (err, user) {
-		if (err) {
-			console.log(err);
-			return res.render("login");
-		}
-		if(user != null && user.password === _password)
-		{
-			console.log("Success!");
-			return res.render("secret");
-		}
+  User.findOne({ username: _username }, function (err, user) {
+    if (err) {
+      console.log(err);
+      return res.render("login");
+    }
+    if (user != null && user.password === _password) {
+      console.log("Success!");
+      return res.render("secret");
+    }
 
-		console.log("Not such user and pass!");
-		return res.render("login");
-	});
-
+    console.log("Not such user and pass!");
+    return res.render("login");
+  });
 });
 
 app.listen(3000, function () {
