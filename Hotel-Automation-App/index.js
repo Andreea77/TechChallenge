69d7089f9d8028eb5f2d7f103b274552cbca1059
pricob <mihai.pricob@yahoo.com>
2021-04-07 18:53:22 +0300

    Update index.js

--- Hotel-Automation-App/index.js
+++ Hotel-Automation-App/index.js
@@ -112,6 +112,6 @@ app.post("/security-code", function (req, res) {
 
 // ---------------------------- //
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Example app listening on port 3000!");
 });
